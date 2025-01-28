7dbcc1dd52fcb98a7bbfbc8ca5f5e56258cbbb8f
kworster <kworster@users.noreply.github.com>
2025-01-28 22:25:55 +0000

    deploy: 233a9aaa41f799fea6cccd593f087d78df487c8d

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import js from "@eslint/js";
+
+export default [
+    {
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "warn"
+        },
+        files: ["*.js"]
+      }
+];
